f9e6d6f95b4b57347d9264e58cb1fa32274fc30d
Alaa Alhazmi <alaaalhazmi-is@hotmail.com>
2020-08-18 23:54:39 +0300

    Error handling & Form Validation

--- src/server/index.js
+++ src/server/index.js
@@ -24,27 +24,30 @@ app.get('/', function (req, res) {
 
 app.post('/sentimentapi', async(req, res) =>{
 
-	let apiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${application_key}&url=${req.body.userUrl}&lang=en`;
+	try {
+		let apiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${application_key}&url=${req.body.userUrl}&lang=en`;
 
-	let response = await fetch(apiUrl);
-	let data = await response.json();
+		let response = await fetch(apiUrl);
+		let data = await response.json();
 
-	if(!response.ok){
-		throw new Error;
+		if(data.status.msg === 'OK'){
+			const evaluation = {};
+			evaluation.agreement = data.agreement;
+			evaluation.confidence = data.confidence;
+			evaluation.irony = data.irony;
+			evaluation.subjectivity = data.subjectivity;
+
+			res.json(evaluation)
+
+		} else {
+			res.json({msg: data.status.msg})
+		}
+	} catch (err) {
+		res.status(500).send({
+	      status: 'error',
+	    })
 	}
-
-	if(data.status.msg === 'OK'){
-		const evaluation = {};
-		evaluation.agreement = data.agreement;
-		evaluation.confidence = data.confidence;
-		evaluation.irony = data.irony;
-		evaluation.subjectivity = data.subjectivity;
-
-		res.json(evaluation)
-
-	} else {
-		res.json({msg: data.status.msg})
-	}
+	
 })
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
